d1ebf552bf8b659024e9a31fffa8cc1c9f52d5cb
Florian Fechner <florian@Florians-MBP.fritz.box>
2021-05-22 18:33:05 +0200

    make symbolbar build ready

--- electron.js
+++ electron.js
@@ -6,7 +6,7 @@ const { globalShortcut, nativeTheme } = require('electron')
 nativeTheme.on("updated", () => 
 {
   const isDark = nativeTheme.shouldUseDarkColors || false
-  const trayIcon = path.resolve(`./img/icons/icon${isDark ? "_dark" : ""}.png`)
+  const trayIcon = path.resolve(`${__dirname}/img/icons/icon${isDark ? "_dark" : ""}.png`)
 
   mb.tray.setImage(trayIcon)
 });
@@ -15,9 +15,9 @@ const isDark = nativeTheme.shouldUseDarkColors || false
 
 const mb = menubar(
 {
-  dir: './public',
+  dir: `${__dirname}/public`,
   preloadWindow: true,
-  icon: `./img/icons/icon${isDark ? "_dark" : ""}.png`,
+  icon: `${__dirname}/img/icons/icon${isDark ? "_dark" : ""}.png`,
   browserWindow: 
   {
     minWidth: 360,
@@ -37,59 +37,3 @@ mb.on('ready', () =>
     mb.window.show()
   })
 })
-
-/*
-// Modules to control application life and create native browser window
-const {app, BrowserWindow, screen} = require('electron')
-const path = require('path')
-const { floor } = Math;
-
-const createWindow = () =>
-{
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-
-  const windowMinWidth = floor(width * 0.3);
-  const windowStartWidth = floor(width * 0.7);
-
-  const windowMinHeight = floor(height * 0.3);
-  const windowStartHeight = floor(height * 0.7);
-
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    minWidth: windowMinWidth,
-    width: windowStartWidth,
-    minHeight: windowMinHeight,
-    height: windowStartHeight,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('public/index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-*/
